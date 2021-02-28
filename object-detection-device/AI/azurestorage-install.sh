d6c2d84458d0ee990ac2d34977deeecae3a38a35
Muhammad Nabeel <munabeel@microsoft.com>
2021-02-28 00:51:41 -0800

    Adding AI code to be run on the device.
    Adding AI code to be run on the device.

---
+++ object-detection-device/AI/azurestorage-install.sh
@@ -0,0 +1,2 @@
+sudo pip3 install azure-storage-queue==12.1.5
+sudo pip3 install azure-storage-blob==12.7.1
---
+++ object-detection-device/AI/eventhub-install-fix.sh
@@ -0,0 +1,7 @@
+sudo apt-get install -y build-essential libssl-dev uuid-dev cmake libcurl4-openssl-dev pkg-config
+
+sudo pip3 install azure-eventhubs
+
+sudo pip3 install azure-eventhub-checkpointstoreblob-aio
+
+
