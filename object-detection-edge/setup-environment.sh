db232fda7cff146c0d890e282289f60827410a55
Muhammad Nabeel <munabeel@microsoft.com>
2021-02-28 02:05:37 -0800

    Delete redundant folder.
    Delete redundant folder.

--- object-detection-edge/setup-environment.sh
+++
@@ -1,3 +0,0 @@
-hub_name="nvidia-deepstream-hub"
-nvidiaJetsonEdgeDevice="object-detection-edge-device"
-az iot hub device-identity create --device-id $nvidiaJetsonEdgeDevice --edge-enabled --hub-name $hub_name
